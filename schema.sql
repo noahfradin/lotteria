34c3d52d804b3f93fcde8803516d8b0724041155
Noah Fradin <noahfradin@noahs-mbp.home>
2014-04-06 18:38:25 -0400

    created initial sql schema

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  photo TEXT,
+  tickets BLOB,
+  pools BLOB,
+  facebook_id TEXT,
+);
+CREATE TABLE tickets(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user INTEGER,
+  numbers TEXT,
+  draw_date TEXT,
+  purchase_date TEXT,
+  power_play INTEGER
+);
+CREATE TABLE pools(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  size INTEGER,
+  users BLOB,
+  tickets BLOB
+);
