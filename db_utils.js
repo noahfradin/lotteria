9ce864e6699391ed40613f9c2fc95ef48b4f8efe
aking <psy_wombats@wombatrpgs.net>
2014-04-09 22:16:01 -0400

    Database utils reformat, table creation

--- db_utils.js
+++ db_utils.js
@@ -1,29 +1,40 @@
 // various database and user management utilities
 
 // stores the user in the database, then calls callback with that user's id
-exports.storeUser = function(user, callback) {
+function storeUser(user, conn, callback) {
   // TODO
   callback(0);
 };
 
 // loads the user from the database, then calls callback with the user object
-exports.loadUser = function(user, callback) {
+function loadUser(user, conn, callback) {
   // TODO
   callback(new Object());
 };
 
 // creates the user from a facebook callback, then calls done
-exports.createUser = function (accessToken, refreshToken, profile, done) {
-  // TODO
-  var user = profile;
-  done(null, user);  // null could be an error object maybe
+// accessToken: what we use to get stuff from FB
+// refreshToken: no idea
+// profile : JSON object representing user basic profile
+// conn: connection to the database
+function createUser(accessToken, refreshToken, profile, done, conn) {
+  var newUser = profile;
+  newUser.accessToken = accessToken;
+  storeUser(newUser, conn, function(user) {
+    done(null, user);
+  });
 };
 
-exports.newTables = function() {
-  conn.query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,first_name TEXT, last_name TEXT, photo TEXT, tickets BLOB,pools BLOB, facebook_id TEXT")
+function newTables(conn) {
+  conn.query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT, last_name TEXT, photo TEXT, tickets BLOB, pools BLOB, facebook_id TEXT)")
   .on('error', console.error);
   conn.query("CREATE TABLE IF NOT EXISTS tickets (id INTEGER PRIMARY KEY AUTOINCREMENT, user INTEGER, numbers TEXT, draw_date TEXT, purchase_date TEXT, power_play INTEGER)")
   .on('error', console.error);
   conn.query("CREATE TABLE IF NOT EXISTS pools (id INTEGER PRIMARY KEY AUTOINCREMENT, size INTEGER, users BLOB, tickets BLOB)")
   .on('error', console.error);
 }
+
+exports.newTables = newTables;
+exports.createUser = createUser;
+exports.loadUser = loadUser;
+exports.storeUser = storeUser;
