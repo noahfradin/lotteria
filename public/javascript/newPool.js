f953c94bcfb73175522df7c6ee085d54048e9808
aking <psy_wombats@wombatrpgs.net>
2014-05-01 22:31:53 -0400

    Image upload now stored correctly

--- public/javascript/newPool.js
+++ public/javascript/newPool.js
@@ -4,13 +4,40 @@ window.addEventListener('load',function(){
     var day = document.getElementById("day");
     var year = document.getElementById("year");
     var description = document.getElementById("ticketDescription");
+    
     month.addEventListener("click", function(month) { deleteMonthDefault();});
     day.addEventListener("click", function(day) { deleteDayDefault();});
     year.addEventListener("click", function(year) { deleteYearDefault();});
     
-    month.addEventListener("keyup", function(month) { addMonthDefault(month,day);});
-    day.addEventListener("keyup", function(day) { addDayDefault(day,year);});
-    year.addEventListener("keyup", function(year) { addYearDefault(year,description);});
+    month.addEventListener("keyup", function(month) { addMonthDefault(month,day); });
+    day.addEventListener("keyup", function(day) { addDayDefault(day,year); });
+    year.addEventListener("keyup", function(year) { addYearDefault(year,description); });
+    
+    var image = document.getElementById("eventImage");
+    var imageHidden = document.getElementById("imageURL");
+    var imageInput = document.getElementById("imageInput");
+    
+    var uploadImage = function() {
+        var file = document.getElementById('imageInput').files[0];
+        if (file) {
+            var xhr = new XMLHttpRequest();
+            var fd = new FormData();
+            fd.append("img", file);
+
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4) {
+                    var json = JSON.parse(xhr.responseText);
+                    image.src = json.url;
+                    imageHidden.value = json.url;
+                }
+            };
+            
+            xhr.open("POST", "/upload/image");
+            xhr.send(fd);
+        }
+    };
+    
+    imageInput.addEventListener("change", uploadImage, false);
 
 },false);
 
